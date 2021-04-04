8f1000f76654b4be28caf1fedfadfc7df6791df2
jeongryul.park <jeongryul.park@navercorp.com>
2021-04-04 20:08:50 +0900

    Complete Chapter 4

--- src/main/java/com/lookchallenge/springboot/service/posts/PostsService.java
+++ src/main/java/com/lookchallenge/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.lookchallenge.springboot.service.posts;
 
 import com.lookchallenge.springboot.domain.posts.Posts;
 import com.lookchallenge.springboot.domain.posts.PostsRepository;
+import com.lookchallenge.springboot.web.dto.PostsListResponseDto;
 import com.lookchallenge.springboot.web.dto.PostsResponseDto;
 import com.lookchallenge.springboot.web.dto.PostsSaveRequestDto;
 
@@ -9,6 +10,9 @@ import com.lookchallenge.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,19 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() ->
                 new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시물이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
